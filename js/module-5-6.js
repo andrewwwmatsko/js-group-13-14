18be66a584226bd0c23a99ddbe55c058d1c92a11
Eva52975 <pavliuk.evelina@gmail.com>
2024-04-21 15:11:16 +0300

    eva

--- js/module-5-6.js
+++ js/module-5-6.js
@@ -17,3 +17,27 @@
 // }
 // const greet = (name) => alert(`Hello ${name}`);
 // console.log(letMeSeeYourName(greet));
+
+//TODO № 2 з колбеком ==============================================
+//Напишіть дві функції
+//makeProduct(name, price, callback) - приймає
+//ім'я та ціну товару, а також callback.
+//Функція створює об'єкт товару, додаючи йому унікальний
+//ідентифікатор як id і викликає callback
+//Передаючи йому створений об'єкт.
+//showProduct(product) - коллбек приймаючий об'єкт
+//продукту і логірующий їх у консоль
+
+// function makeProduct(name, price, callback) {
+//   const product = {
+//     name,
+//     price,
+//     id: Math.random(),
+//   };
+//   return callback(product);
+// }
+
+// const showProduct = (product) => product;
+
+// console.log(makeProduct("fish", 200, showProduct));
+// console.log(makeProduct("orange", 267, showProduct));
