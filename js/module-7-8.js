3df81911e4f6424ca9aab1317530ec6309cbae00
Keeldg <154369545+Keeldg@users.noreply.github.com>
2024-05-05 17:01:41 +0300

    7-8 task03

--- js/module-7-8.js
+++ js/module-7-8.js
@@ -45,3 +45,16 @@
 //   const toggleTextBtn = textBtn === "Приховати" ? "Розкрити" : "Приховати";
 //   btn.textContent = toggleTextBtn;
 // }
+
+//TODO: № 3 ==============================================
+// При кліку на кнопку текст параграфів змінюється на їх порядковий номер
+
+const btn = document.querySelector("button");
+const peshki = document.querySelectorAll("p");
+
+btn.addEventListener("click", onClickHeandler);
+function onClickHeandler() {
+  peshki.forEach((p, index) => {
+    p.textContent = index + 1;
+  });
+}
